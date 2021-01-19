85d0ac7bfca1f08b397dd3ae7511befab94b6856
yuruweimu@163.com <huangxi7832452>
2021-01-19 16:08:19 +0800

    2

--- src/js/index.vue
+++ src/js/index.vue
@@ -9,7 +9,7 @@
       multiple>
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过1500kb</div>
     </el-upload>
   </div>
 </template>
